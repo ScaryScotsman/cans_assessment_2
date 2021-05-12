bf185e6419404184512fe25845fce380648776ad
Michael Mcgrattan <mcgrattanmichael@gmail.com>
2021-05-12 16:27:07 +0100

    MMcG 12/05/21 - Implementing client logic and functionality

--- client.py
+++ client.py
@@ -8,7 +8,10 @@ cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cli_sock.connect((sys.argv[1], int(sys.argv[2])))
 
 # Send request back to server
-cli_sock.sendall(sys.argv[3].encode('utf-8'))
+cli_sock.send(sys.argv[3].encode() + b' ')
+
+if sys.argv[3] == "PUT" or sys.argv[3] == "GET":
+    cli_sock.send(sys.argv[4].encode())
 
 # Closing socket connection
 cli_sock.close()
