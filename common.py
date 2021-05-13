92dd8c22cf7163edaa823a8bdb33e8c8b5c3d1a4
Michael Mcgrattan <mcgrattanmichael@gmail.com>
2021-05-13 18:35:54 +0100

    MMcG 13/05/21 - Refactoring common functions file to provide more detailed exception logs

--- common.py
+++ common.py
@@ -1,32 +1,32 @@
 def writeFile(addr, filepath, fileData):
     responseMessage = str(addr)
 
+
+def writeFile(responseMessage, filepath, fileData):
     try:
         with open(file=filepath, mode='xb') as uploadF:
             for line in fileData:
                 uploadF.write(line)
 
-        responseMessage += ": SUCCESS - file has been sent successfully"
-    except FileExistsError:
-        responseMessage += ": FAILURE - file already exists"
-    except:
-        responseMessage += ": FAILURE - something has gone wrong"
+        responseMessage += ": SUCCESS - file has been sent successfully. \n"
+    except FileExistsError as e:
+        responseMessage += ": FAILURE - file already exists. \n " + str(e)
+    except Exception as e:
+        responseMessage += ": FAILURE - something has gone wrong. \n" + str(e)
     finally:
         print(responseMessage)
 
 
-def readFile(addr, requestFilePath):
-    responseMessage = addr
-
+def readFile(responseMessage, requestFilePath):
     try:
         requestFile = open(file=requestFilePath, mode='rb')
         requestFileData = requestFile.readlines()
         return requestFileData
 
-        responseMessage += ": SUCCESS - file data read successfully"
-    except FileNotFoundError:
-        responseMessage += ": FAILURE - file doesn't exist"
-    except:
-        responseMessage += ": FAILURE - something has gone wrong"
+        responseMessage += ": SUCCESS - file data read successfully. \n"
+    except FileNotFoundError as e:
+        responseMessage += ": FAILURE - file doesn't exist. \n " + str(e)
+    except Exception as e:
+        responseMessage += ": FAILURE - something has gone wrong. \n " + str(e)
     finally:
         print(responseMessage)
