7f86432e78d789ad872780155369257f723f13fb
Michael Mcgrattan <mcgrattanmichael@gmail.com>
2021-05-13 18:32:32 +0100

    MMcG 13/05/21 - Refactoring client to handle connection error exceptions

--- client.py
+++ client.py
@@ -3,15 +3,31 @@ import sys
 
 # Creating socket
 cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+responseMessage = ""
 
 # Connecting socket to server
-cli_sock.connect((sys.argv[1], int(sys.argv[2])))
+try:
+    cli_sock.connect((sys.argv[1], int(sys.argv[2])))
+    responseMessage = str(cli_sock.getpeername()) + ": SUCCESS - connected to server. \n"
 
-# Send request back to server
-cli_sock.send(sys.argv[3].encode() + b' ')
+    # Send request back to server
+    if sys.argv[3].lower() == "put" or sys.argv[3].lower() == "get":
+        cli_sock.sendall(sys.argv[3].encode() + b' ' + sys.argv[4].encode())
+    else:
+        cli_sock.sendall(sys.argv[3].encode() + b' ')
 
-if sys.argv[3] == "PUT" or sys.argv[3] == "GET":
-    cli_sock.send(sys.argv[4].encode())
+except (ConnectionError, ConnectionAbortedError, ConnectionRefusedError):
+    responseMessage = "FALIURE - couldn't connect to server. \n"
 
-# Closing socket connection
-cli_sock.close()
+except Exception as e:
+    responseMessage = "FAILURE - something has gone wrong. \n " + str(e)
+
+finally:
+    print(responseMessage)
+
+    # Closing socket connection
+    cli_sock.close()
+
+
+
+
